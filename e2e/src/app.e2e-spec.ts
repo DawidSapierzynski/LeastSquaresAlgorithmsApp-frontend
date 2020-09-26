8be4fd60aad9957d2b8577d8271dbd95cd9535c3
Dawid Sapierzyński <dawid.sapierzynski@student.wat.edu.pl>
2020-09-26 13:46:21 +0200

    Name changed to LeastSquaresAlgorithmsApp-frontend

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('AutoApproximationApp-frontend app is running!');
+    expect(page.getTitleText()).toEqual('LeastSquaresAlgorithmsApp-frontend app is running!');
   });
 
   afterEach(async () => {
