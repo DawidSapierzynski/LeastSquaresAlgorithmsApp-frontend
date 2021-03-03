a1ea869f09472e64f1e96e6653d21b022b035b4a
Dawid Sapierzyński <dawid.sapierzynski@student.wat.edu.pl>
2021-03-03 18:51:04 +0100

    Adding selection of methods for generating the X coordinate of points

--- src/app/dto/GenerateDataSeriesForm.ts
+++ src/app/dto/GenerateDataSeriesForm.ts
@@ -4,6 +4,7 @@ export class GenerateDataSeriesForm {
   public mathematicalFunctionDTO: MathematicalFunctionDTO;
   public numberPoints: number;
   public weightDistribution: string;
+  public distanceX: string;
 
   constructor() {
   }
