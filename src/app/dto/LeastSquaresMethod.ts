87a4541f3cf1ee174a5fbd624f9278f987fa3e0f
Dawid Sapierzyński <dawid.sapierzynski@student.wat.edu.pl>
2021-04-23 11:59:30 +0200

    Fix Normalization

--- src/app/dto/LeastSquaresMethod.ts
+++ src/app/dto/LeastSquaresMethod.ts
@@ -1,5 +1,5 @@
 export enum LeastSquaresMethod {
-  STANDARDIZATION = 'Standardization',
+  NORMALIZATION = 'Normalization',
   HOUSEHOLDER_TRANSFORMATION = 'Householder Transformation',
   GIVENS_ROTATION = 'Givens Rotation',
   SINGULAR_VALUE_DECOMPOSITION = 'Singular Value Decomposition'
