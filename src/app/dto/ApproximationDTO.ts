399d99cfbdc3a85ffb7f9e806a8b88da87b57da7
Dawid Sapierzyński <dawid.sapierzynski@student.wat.edu.pl>
2021-05-17 10:25:34 +0200

    Add rSquared

--- src/app/dto/ApproximationDTO.ts
+++ src/app/dto/ApproximationDTO.ts
@@ -5,10 +5,12 @@ export class ApproximationDTO {
   public mathematicalFunctionDTOs: MathematicalFunctionDTO[];
   public points: PointXY[];
   public absoluteError: number;
+  public rsquared: number;
 
-  constructor(mathematicalFunctionDTOs: MathematicalFunctionDTO[], points: PointXY[], absoluteError: number) {
+  constructor(mathematicalFunctionDTOs: MathematicalFunctionDTO[], points: PointXY[], absoluteError: number, rSquared: number) {
     this.mathematicalFunctionDTOs = mathematicalFunctionDTOs;
     this.points = points;
     this.absoluteError = absoluteError;
+    this.rsquared = rSquared;
   }
 }
--- src/app/dto/ApproximationView.ts
+++ src/app/dto/ApproximationView.ts
@@ -5,10 +5,12 @@ export class ApproximationView {
   public mathematicalFunctionDTOs: MathematicalFunctionDTO[];
   public method: LeastSquaresMethod;
   public absoluteError: number;
+  public rSquared: number;
 
-  constructor(mathematicalFunctionDTOs: MathematicalFunctionDTO[], method: LeastSquaresMethod, absoluteError: number) {
+  constructor(mathematicalFunctionDTOs: MathematicalFunctionDTO[], method: LeastSquaresMethod, absoluteError: number, rSquared: number) {
     this.mathematicalFunctionDTOs = mathematicalFunctionDTOs;
     this.method = LeastSquaresMethod[method];
     this.absoluteError = absoluteError;
+    this.rSquared = rSquared;
   }
 }
